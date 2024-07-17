06c22a3c1c5fbc90fb5310ed23dc1aee5cda0313
2113ic <icxiaoyanz@gmail.com>
2024-07-17 15:15:31 +0800

    init repo

---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import type { ApiTypeMap, ExtraOptions, URLMatchGroup, ErrorReturn, UseFetchReturn, UseIFetchReturn } from './type'
+import type { AxiosInstance } from 'axios'
+import { ref, shallowRef } from '@vue/reactivity'
+
+export default function createFetch(http: AxiosInstance) {
+  function useFetch<
+    T extends ApiTypeMap[P],
+    P extends keyof ApiTypeMap,
+  >(url: P, options: Partial<ExtraOptions<T, P>> = {}): UseFetchReturn<T['response']> {
+
+    const { method, path } = patchURL<T, P>(url, options.params)
+    options = { immediate: true, method, url: path, ...options }
+
+    const data = shallowRef<T['response'] | null>(options.default ?? null)
+    const error = shallowRef<ErrorReturn | null>(null)
+    const isloading = ref(false)
+
+    options.immediate && reload()
+    function reload() {
+      isloading.value = true
+
+      const response = http.request({
+        url: options.url,
+        method: options.method,
+        data: options.body,
+      })
+
+      response.then((r: any) => data.value = r.data)
+      response.catch((e: any) => error.value = e)
+      response.finally(() => isloading.value = false)
+    }
+
+    return { data, error, isloading, reload }
+  }
+
+  async function useIFetch<
+    T extends ApiTypeMap[P],
+    P extends keyof ApiTypeMap,
+  >(url: P, options: Partial<ExtraOptions<T, P>> = {}): Promise<UseIFetchReturn<T['response']>> {
+
+    const { method, path } = patchURL<T, P>(url, options.params)
+    options = { immediate: true, method, url: path, ...options }
+
+    let data: T['response'] | null = options.default ?? null
+    let error: ErrorReturn | null = null
+
+    options.immediate && await reload()
+    async function reload() {
+
+      try {
+        const response = await http.request({
+          url: options.url,
+          method: options.method,
+          data: options.body,
+        })
+
+        data = response.data
+      }
+      catch (e: any) {
+        error = e
+      }
+    }
+
+    return { data, error, reload }
+  }
+
+  return { useFetch, useIFetch }
+}
+
+function patchURL<
+  T extends ApiTypeMap[P],
+  P extends keyof ApiTypeMap,
+>(url: string, params?: T['request']) {
+  const matchs = url.match(/(?<method>GET|POST|PUT|DELETE)? *(?<path>.+)/)
+  if (!matchs) throw new Error('URL Wrong Format!')
+
+  const { method, path } = (matchs.groups as any) as URLMatchGroup
+  let _path = path
+
+  if (params) {
+    if (typeof params !== 'object') {
+      _path = path.replace(/:([\w-]+)/, String(params))
+    }
+    else {
+      for (const [key, value] of Object.entries(params)) {
+        const reg = new RegExp(`[:{]${key}\}?`)
+        _path = _path.replace(reg, String(value))
+      }
+      _path = _path.replace(/[:{][\w-]+\}?/g, '')
+    }
+  }
+
+  return { method, path: _path }
+}
---
+++ src/type.ts
@@ -0,0 +1,60 @@
+import { Ref } from "@vue/reactivity"
+
+export interface ApiTypeMap {}
+
+export interface URLMatchGroup {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  path: string
+}
+
+export interface ExtraOptions<T extends ApiTypeMap[P], P extends keyof ApiTypeMap> {
+  /**
+   * Request URL
+   */
+  url: string
+
+  /**
+   * Request Method
+   */
+  method: URLMatchGroup['method']
+
+  /**
+   * Request path params
+   * @example /api/user/:id
+   */
+  params: T['request']
+
+  /**
+   * POST Request body params
+   */
+  body: T['request']
+
+  /**
+   * Wherevent immediate request
+   */
+  immediate: boolean
+
+  /**
+   * Data default value
+   */
+  default: T['response']
+}
+
+export interface ErrorReturn {
+  code: number
+  message: string
+  data: null
+}
+
+export interface UseFetchReturn<T> {
+  data: Ref<T | null>
+  error: Ref<ErrorReturn | null>
+  isloading: Ref<boolean>
+  reload: () => void
+}
+
+export interface UseIFetchReturn<T> {
+  data: T | null
+  error: ErrorReturn | null
+  reload: () => Promise<void>
+}
